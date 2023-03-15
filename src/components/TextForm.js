a9e4fbf712acb46be950056c2abf503200a881d0
Sachin Kumar <mail.sachin161@gmail.com>
2023-03-15 21:51:23 +0530

    App Phase1 completed

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -14,9 +14,7 @@ const TextFrom = ({ mode, setMode, setShowAlert, setAlertText }) => {
   };
 
   const handleCopy = () => {
-    let text = document.getElementById("myBox");
-    text.select();
-    navigator.clipboard.writeText(text.value);
+    navigator.clipboard.writeText(text);
     setAlertText("Text Copied!!");
     setShowAlert(true);
     setTimeout(() => {
@@ -112,7 +110,7 @@ const TextFrom = ({ mode, setMode, setShowAlert, setAlertText }) => {
       >
         Total Words =
         {
-          text.split(" ").filter((e) => {
+          text.split(/\s+/).filter((e) => {
             return e.length !== 0;
           }).length
         }
